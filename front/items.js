f251808646842d56634a8240a3c95788afb0b384
fmartinPioIX <fmartin@pioix.edu.ar>
2026-06-08 10:07:17 -0300

    clases y poquito html
    hicimos cosas

---
+++ front/items.js
@@ -0,0 +1,8 @@
+class Item{
+    constructor(name,imgsrc,price){
+        this.name=name
+        this.imgsrc=imgsrc
+        this.price=price
+        this.id=id
+    }
+}
---
+++ front/usuarios.js
@@ -0,0 +1,10 @@
+class Usuario{
+    constructor(username,password){
+        this.username=username
+        this.password=password
+        this.is_admin=false
+        this.points=0
+        this.id=id
+    }
+}
+
