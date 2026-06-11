63a682b574658aa40be01fc8aa324f2018d2430a
fmartinPioIX <fmartin@pioix.edu.ar>
2026-06-11 08:39:01 -0300

    handleRegister y login avanzados

--- front/logic.js
+++ front/logic.js
@@ -1,21 +1,38 @@
 let users = []
-
+let UserLogged = {} // despues newuser.idUser
 
 
 function handleSignup(){
     const username = prompt("Ingrese su nombre de usuario deseado...");
+    while (username == ""){
+        const username = prompt("Llena los campos...")
+    }
     for (i=0; i<users.length; i++) {
         while (username == users[i].username){
             const username = prompt("Nombre de usuario en uso. Por favor, ingrese uno nuevo...")
         }
     }
     const password = prompt("Ingrese su contraseña deseada...")
+    while (password == ""){
+        const password = prompt("Llena los campos...")
+    }
+    signup()
 }
 
-function logicLogin(username,password){
-
+function login(username,password){
+    for (let i = 0; i < users.length; i++) {
+        if (username == users[i].username) {
+            if (password == users[i].password) {
+                return users[i].idUser
+            } else {
+                return 0
+            }
+        }
+    }
 }
 
 function handleLogin(username,password){
-    
+}
+
+function signup(username,password) {
 }
