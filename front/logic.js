95c552857cd46fcefd11a908613c6a0d7d2b74a0
Franco Martin Moratti <fmartin@pioix.edu.ar>
2026-06-17 08:44:03 -0300

    estructura nueva (incompleto)
    login ahora usa fetch en parte

--- front/logic.js
+++ front/logic.js
@@ -1,38 +1,53 @@
-let users = []
 let UserLogged = {} // despues = objeto de usuario loggeado
 
 
 function handleSignup(){
     const username = prompt("Ingrese su nombre de usuario deseado...");
-    while (username == ""){
-        const username = prompt("Llena los campos...")
-    }
-    for (i=0; i<users.length; i++) {
-        while (username == users[i].username){
-            const username = prompt("Nombre de usuario en uso. Por favor, ingrese uno nuevo...")
+    if (username == "" || getUsuarioporUsername(username).length !== 0) {
+        const username = prompt("Valor en uso o vacio. Por favor, ingrese uno nuevo...")
+    } else {
+        const password = prompt("Ingrese su contraseña deseada...")
+        if (password == ""){
+            const password = prompt("Valor vacio. Completelo...")
         }
     }
-    const password = prompt("Ingrese su contraseña deseada...")
-    while (password == ""){
-        const password = prompt("Llena los campos...")
-    }
-    signup()
+
+    // let idUsuario = login(username, password)
+    // if (idUsuario = 0) {
+    //     return 0
+    // } else {
+    //     let newUsuario = new Usuario(idUser)
+    //     users.push(newUsuario)
+    //     return newUsuario.idUser
+    // }
 }
 
 function login(username,password){
     for (let i = 0; i < users.length; i++) {
         if (username == users[i].username) {
             if (password == users[i].password) {
-                return users[i].idUser
+                return users[i].idUser /*<-- hacer que retorne id del logeado*/
             } else {
                 return 0
             }
         }
     }
+    return -1
 }
 
 function handleLogin(username,password){
+    const username = prompt("Ingrese su nombre de usuario...")
+    const password = prompt("Ingrese la contraseña del usuario...")
+    
+    let idUsuario = login(username, password)
+    if (idUsuario == -1) {
+        alert("Usuario no reconocido")
+    } else if (idUsuario == 0) {
+        alert("Una contraseña mas mi gente")
+    } else {
+        UserLogged = 
+    }
 }
 
-function signup(username,password) {
-}
+// el array users es inutil porque tenemos fetch de base de datos. Usa eso como "getUsuarioporUsername(username)"
+//if getUsuarioporUsername() == 0 (para ver si retorna algo en caso de logear algo que no existe)
