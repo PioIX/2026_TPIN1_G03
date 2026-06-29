a7ebd8f881727453bc9056707e25c9ac4410f078
fmartinPioIX <fmartin@pioix.edu.ar>
2026-06-29 10:09:01 -0300

    fixes y funciones sin acabar

--- front/admin.js
+++ front/admin.js
@@ -6,6 +6,7 @@ async function verUsers() {
         <th>ID</th>
         <th>Username</th>
         <th>Points</th>
+        <th>Items</th>
     </tr>`
     console.log(tabla)
     for (let i = 0; i < tabla.length; i++) {
@@ -15,6 +16,7 @@ async function verUsers() {
             <td>${element.id}</td>
             <td>${element.username}</td>
             <td>${element.points}</td>
+            <td>${element.items}</td>
         </tr>
         `;        
     }
@@ -26,25 +28,41 @@ async function modPuntosUser() {
     let idAdmin = prompt("¿Que ID de usuario desea alterar?")
     objetoUsuarioAux = await getUsuarioporID(idAdmin)
     objetoUsuario = objetoUsuarioAux[0]
-    if (idAdmin != "") {
-        let newPuntos = prompt("Escriba el nuevo puntaje total de " + objetoUsuario.username + ".")
-        if (newPuntos != "") {
-            objetoUsuario.points = newPuntos
-            alert("El nuevo puntaje de " + objetoUsuario.username + " es " + objetoUsuario.points + ".")
-        } else {
-            alert("Conjunto vacio!")
-        }
+    while (idAdmin == "" || objetoUsuario.length == 0) {
+        idAdmin = prompt("Complete con un ID valido...")
     }
+    let newPuntos = prompt("Escriba el nuevo puntaje total de " + objetoUsuario.username + ".")
+    while (newPuntos == "") {
+        newPuntos = prompt("El campo esta vacio, completelo...")
+    }
+    objetoUsuario.points = newPuntos
+    alert("El nuevo puntaje de " + objetoUsuario.username + " es " + objetoUsuario.points + ".")
 }
 
 async function cheatPuntos() {
     let idAdmin = prompt("¿Que ID de usuario desea alterar?")
+    objetoUsuarioAux = await getUsuarioporID(idAdmin)
+    objetoUsuario = objetoUsuarioAux[0]
     console.log(objetoUsuario)
     while (idAdmin == "" || objetoUsuario.length == 0) {
         idAdmin = prompt("Complete con un ID valido...")
     }
     objetoUsuario.points += 500
-    console.log("name: ",objetoUsuario.username)
-    console.log("score: ",objetoUsuario.points)
+    console.log("name: ", objetoUsuario.username)
+    console.log("score: ", objetoUsuario.points)
     alert("El nuevo puntaje de " + objetoUsuario.username + " es " + objetoUsuario.points + ".")
+}
+
+async function cheatItems() {
+    let idAdmin = prompt("¿Que ID de usuario desea alterar?")
+    objetoUsuarioAux = await getUsuarioporID(idAdmin)
+    objetoUsuario = objetoUsuarioAux[0]
+    console.log(objetoUsuario)
+    while (idAdmin == "" || objetoUsuario.length == 0) {
+        idAdmin = prompt("Complete con un ID valido...")
+    }
+    const todosLosItems = await getUsuarios()
+    objetoUsuario.items = todosLosItems
+    console.log(objetoUsuario.items)
+    alert("Todos los items añadidos a " + objetoUsuario.username + ".")
 }
